75696cd864e0b01d2b8952fb6e8118218179bfc2
Nikita Plakhin <nplkhn1@yandex.ru>
2019-02-19 00:31:00 +0300

    all tasks done

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;
+  for (let i = 0; i < preferences.length; i++) {
+    if (preferences[preferences[preferences[i] - 1] - 1] - 1 == i) {
+      count++;
+    }
+  }
+  return Math.floor(count / 3);
 };
